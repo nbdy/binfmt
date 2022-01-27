f46c6b2afcf7241189b1cccc4d3790a37f1bd74c
Pascal Eberlein <pascal@eberlein.io>
2022-01-27 20:55:23 +0100

    Using FunctionTimer library for benchmarks

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -2,25 +2,8 @@
 // Created by nbdy on 02.09.21.
 //
 
+#include "FunctionTimer/FunctionTimer.h"
 #include "test_common.h"
-
-TestBinaryFile getRandomTestFile() {
-  TestBinaryFile r(TEST_BINARY_FILE, TestBinaryFileHeader{});
-  EXPECT_TRUE(Fs::exists(r.getFilePath()));
-  return r;
-}
-
-TestBinaryEntryContainer generateRandomTestEntryContainer() {
-  return TestBinaryEntryContainer(TestBinaryEntry{
-      generateRandomInteger(),
-      generateRandomFloat(),
-  });
-}
-
-void cleanupTestFile(TestBinaryFile f) {
-  f.deleteFile();
-  EXPECT_FALSE(Fs::exists(f.getFilePath()));
-}
 
 // NOLINTNEXTLINE(cert-err58-cpp)
 TEST(BinaryFile, test1kInsert) {
@@ -29,13 +12,11 @@ TEST(BinaryFile, test1kInsert) {
   for (uint32_t i = 0; i < 1000; i++) {
     entries.push_back(generateRandomTestEntryContainer().entry);
   }
-  TIMEIT_START("1kWrite")
-  auto r = t.append(entries);
-  TIMEIT_END
-  TIMEIT_RESULT
-  EXPECT_TRUE(r.ok);
-  EXPECT_EQ(t.getFileSize(), 1000 * sizeof(TestBinaryEntryContainer) +
-                                 sizeof(TestBinaryFileHeader));
+  FunctionTimer ft(
+      [&t, entries]() { EXPECT_EQ(t.append(entries), binfmt::ErrorCode::OK); });
+  std::cout << "1k insert: " << ft.getExecutionTimeMs() << "ms" << std::endl;
+  EXPECT_EQ(t.getFileSize(),
+            1000 * sizeof(TestBinaryEntryContainer) + sizeof(TestBinaryHeader));
   cleanupTestFile(t);
 }
 
@@ -46,13 +27,11 @@ TEST(BinaryFile, test10kInsert) {
   for (uint32_t i = 0; i < 10000; i++) {
     entries.push_back(generateRandomTestEntryContainer().entry);
   }
-  TIMEIT_START("10kWrite")
-  auto r = t.append(entries);
-  TIMEIT_END
-  TIMEIT_RESULT
-  EXPECT_TRUE(r.ok);
+  FunctionTimer ft(
+      [&t, entries]() { EXPECT_EQ(t.append(entries), binfmt::ErrorCode::OK); });
+  std::cout << "10k insert: " << ft.getExecutionTimeMs() << "ms" << std::endl;
   EXPECT_EQ(t.getFileSize(), 10000 * sizeof(TestBinaryEntryContainer) +
-                                 sizeof(TestBinaryFileHeader));
+                                 sizeof(TestBinaryHeader));
   int ec = 0;
   std::vector<TestBinaryEntryContainer> allEntries;
   EXPECT_TRUE(t.getAllEntries(allEntries));
@@ -70,13 +49,11 @@ TEST(BinaryFile, test100kInsert) {
   for (uint32_t i = 0; i < 100000; i++) {
     entries.push_back(generateRandomTestEntryContainer().entry);
   }
-  TIMEIT_START("100kWrite")
-  auto r = t.append(entries);
-  TIMEIT_END
-  TIMEIT_RESULT
-  EXPECT_TRUE(r.ok);
+  FunctionTimer ft(
+      [&t, entries]() { EXPECT_EQ(t.append(entries), binfmt::ErrorCode::OK); });
+  std::cout << "100k insert: " << ft.getExecutionTimeMs() << "ms" << std::endl;
   EXPECT_EQ(t.getFileSize(), 100000 * sizeof(TestBinaryEntryContainer) +
-                                 sizeof(TestBinaryFileHeader));
+                                 sizeof(TestBinaryHeader));
   cleanupTestFile(t);
 }
 
@@ -87,24 +64,23 @@ TEST(BinaryFile, test1MInsert) {
   uint32_t insertCount = 10000000;
   std::cout << "Generating file of size "
             << std::to_string(insertCount * sizeof(TestBinaryEntryContainer) +
-                              sizeof(TestBinaryFileHeader))
+                              sizeof(TestBinaryHeader))
             << std::endl;
   for (uint32_t i = 0; i < insertCount; i++) {
     entries.push_back(generateRandomTestEntryContainer().entry);
   }
-  TIMEIT_START("1MWrite")
-  auto r = t.append(entries);
-  TIMEIT_END
-  TIMEIT_RESULT
-  EXPECT_TRUE(r.ok);
+  FunctionTimer ft(
+      [&t, entries]() { EXPECT_EQ(t.append(entries), binfmt::ErrorCode::OK); });
+  std::cout << "1M insert: " << ft.getExecutionTimeMs() << "ms" << std::endl;
   EXPECT_EQ(t.getFileSize(), insertCount * sizeof(TestBinaryEntryContainer) +
-                                 sizeof(TestBinaryFileHeader));
+                                 sizeof(TestBinaryHeader));
   int ec = 0;
   std::vector<TestBinaryEntryContainer> allEntries;
-  TIMEIT_START("1MRead")
-  EXPECT_TRUE(t.getAllEntries(allEntries));
-  TIMEIT_END
-  TIMEIT_RESULT
+  FunctionTimer ft1(
+      [&t, &allEntries]() { EXPECT_TRUE(t.getAllEntries(allEntries)); });
+  std::cout << "1M getAllEntries: " << ft1.getExecutionTimeMs() << "ms"
+            << std::endl;
+
   for (const auto &entry : allEntries) {
     EXPECT_EQ(entry.checksum, TestBinaryEntryContainer(entries[ec]).checksum);
     ec++;
